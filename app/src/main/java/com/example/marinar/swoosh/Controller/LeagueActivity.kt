db749337246ea09191a9c1932f58590465c60268
Marina Romanova <mrnromanova@gmail.com>
2019-08-01 00:08:37 +0200

    added instance state and app icon

--- app/src/main/java/com/example/marinar/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/marinar/swoosh/Controller/LeagueActivity.kt
@@ -14,6 +14,18 @@ class LeagueActivity : AppCompatActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
